1cf6d5e31b4308b96a1d39758aef304ab9ba5c5b
joao.venancio <joao.venancio@mv.com.br>
2023-04-22 08:57:35 -0300

    aula 7 concluida

---
+++ app/src/main/java/com/example/chatappwithfirebase/activities/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chatappwithfirebase.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatappwithfirebase.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
